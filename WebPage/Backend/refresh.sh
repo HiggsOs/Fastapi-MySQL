1160567de201a615f324d85a73051a5a17f7aaa8
LGsus <168134206+HiggsOs@users.noreply.github.com>
2024-09-18 00:09:01 -0500

    Update refresh.sh

--- WebPage/Backend/refresh.sh
+++ WebPage/Backend/refresh.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
 
 
-# Ejecuta git pull para actualizar el repositorio
-git pull origin main
 
-# Verifica si el comando git pull fue exitoso
-if [ $? -eq 0 ]; then
-  echo "Repositorio actualizado correctamente."
-else
-  echo "Error al actualizar el repositorio."
-  exit 1
-fi
+pm2 reload WebPage
+
+
