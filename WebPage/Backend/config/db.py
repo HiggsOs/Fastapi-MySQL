b34e64831a38a69f47c766f24360da5e1165268d
LGsus <168134206+HiggsOs@users.noreply.github.com>
2024-09-18 00:30:51 -0500

    Update db.py

--- WebPage/Backend/config/db.py
+++ WebPage/Backend/config/db.py
@@ -1,6 +1,7 @@
 from sqlalchemy import create_engine,MetaData
 from sqlalchemy.orm import sessionmaker
-from config.credentials import db_credential
+import os
+valor = os.getenv('keyDB')
 
 engine=create_engine(db_credential)
 meta =MetaData()
