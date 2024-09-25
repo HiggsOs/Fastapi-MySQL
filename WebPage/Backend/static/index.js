1d28c78a1e8ad568be8bc3540ed5cbb957042800
Cristian <cristian.vasco835@gmail.com>
2024-09-24 23:37:11 -0500

    jump between tabs fix

--- WebPage/Backend/static/index.js
+++ WebPage/Backend/static/index.js
@@ -60,18 +60,19 @@ window.onload = function() {
 
     fetchData(); // Ejecutar fetchData al cargar la página
 
+    const historicosBtn = document.getElementById("historicos-btn");
+    
+    // Agregar evento de clic
+    historicosBtn.addEventListener("click", function() {
+        // Obtener la URL actual
+        const currentURL = window.location.href;
+        
+        // Crear la nueva URL reemplazando el complemento
+        const newURL = currentURL.replace(/[^/]*$/, "historicos.html");
+        
+        // Redirigir a la nueva URL
+        window.location.href = newURL;
+    });
 };
 
-const historicosBtn = document.getElementById("historicos-btn");
-    
-// Agregar evento de clic
-historicosBtn.addEventListener("click", function() {
-    // Obtener la URL actual
-    const currentURL = window.location.href;
-    
-    // Crear la nueva URL reemplazando el complemento
-    const newURL = currentURL.replace(/[^/]*$/, "historicos.html");
-    
-    // Redirigir a la nueva URL
-    window.location.href = newURL;
-});
+
