f71785dd457e07843c5457f56e8792e07e686889
Cristian <cristian.vasco835@gmail.com>
2024-11-18 16:25:58 -0500

    con esto termino

--- WebPage/Backend/static/index.js
+++ WebPage/Backend/static/index.js
@@ -153,6 +153,7 @@ function actualizarDatosEnPantalla(placa) {
             document.getElementById(`hour-${panelId}`).textContent = vehiculo.data.hour || 'No disponible';
             document.getElementById(`RPM-${panelId}`).textContent = vehiculo.data.rpm || 'No disponible';
             document.getElementById(`speed-${panelId}`).textContent = vehiculo.data.speed || 'No disponible';
+            document.getElementById(`vehicle-color-box-${panelId}`).style.backgroundColor = vehiculo.color || '';
         });
     } else {
         const vehiculo = vehiculos[placa];
@@ -163,6 +164,7 @@ function actualizarDatosEnPantalla(placa) {
         document.getElementById('hour-1').textContent = vehiculo.data.hour || 'No disponible';
         document.getElementById('RPM-1').textContent = vehiculo.data.rpm || 'No disponible';
         document.getElementById('speed-1').textContent = vehiculo.data.speed || 'No disponible';
+        document.getElementById(`vehicle-color-box-1`).style.backgroundColor = vehiculo.color || '';
     }
 }
 
